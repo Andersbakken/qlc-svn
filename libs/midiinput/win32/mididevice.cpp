0385d4f0dd30972815cea7fadd3cfc556fcf40b8
hjunnila <hjunnila@0db28fee-5c31-0410-96ea-e69cce70e049>
2010-09-13 21:17:19 +0000

    Fixed a crash bug related to nonexistent feedback line.

--- libs/midiinput/win32/mididevice.cpp
+++ libs/midiinput/win32/mididevice.cpp
@@ -251,8 +251,14 @@ QString MIDIDevice::infoText()
 		info += QString("Device is working correctly.");
 		info += QString("</P>");
 		info += QString("<P>");
-		info += QString("<B>Feedback Line: </B>%1<BR>")
-				.arg(feedBackNames()[feedBackId()]);
+
+		QStringList fbNames(feedBackNames());
+		if (feedBackId() < UINT(fbNames.size()))
+			info += QString("<B>Feedback Line: </B>%1<BR>")
+					.arg(feedBackNames()[feedBackId()]);
+		else
+			info += QString("<B>Feedback Line: </B>None<BR>");
+
 		info += QString("<B>MIDI Channel: </B>%1<BR>")
 				.arg(m_midiChannel + 1);
 		info += QString("<B>Mode: </B>%1")
