089cfed8381af8ad238fc1fe76955c7b28692e50
hjunnila <hjunnila@0db28fee-5c31-0410-96ea-e69cce70e049>
2010-09-16 19:43:06 +0000

    Updated coverage script to generate a combined report of all tests.

--- coverage.sh
+++ coverage.sh
@@ -1,60 +1,65 @@
 #!/bin/bash
 
+# Remove previous data
+if [ -d coverage ]; then
+	rm -rf coverage
+fi
+
+# Create directories for new coverage data
+mkdir -p coverage/html
+
 #############################################################################
 # Engine
 #############################################################################
-mkdir -p engine/coverage/html
-
-lcov -d engine/src -c -i -o engine/coverage/enginebase.info
-
+lcov -d engine/src -c -i -o coverage/enginebase.info
 pushd .
 cd engine/test
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../src ./test_engine
 popd
+lcov -d engine/src -c -o coverage/enginetest.info
 
-lcov -d engine/src -c -o engine/coverage/enginetest.info
-lcov -a engine/coverage/enginebase.info \
-	-a engine/coverage/enginetest.info \
-	-o engine/coverage/enginemerge.info
-genhtml -o engine/coverage/html engine/coverage/enginemerge.info
+lcov	-a coverage/enginebase.info -a coverage/enginetest.info \
+	-o coverage/enginemerge.info
 
 #############################################################################
 # Enttec DMXUSB
 #############################################################################
-mkdir -p plugins/enttecdmxusbout/unix/coverage/html
-
 # DMXUSB Open
-lcov -d plugins/enttecdmxusbout/unix/src -c -i -o plugins/enttecdmxusbout/unix/coverage/openbase.info
+lcov -d plugins/enttecdmxusbout/unix/src -c -i -o coverage/dmxusbopenbase.info
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:plugins/enttecdmxusbout/unix/src plugins/enttecdmxusbout/unix/test/open/test_dmxusbopen
-lcov -d plugins/enttecdmxusbout/unix/src -c -o plugins/enttecdmxusbout/unix/coverage/opentest.info
+lcov -d plugins/enttecdmxusbout/unix/src -c -o coverage/dmxusbopentest.info
 
-lcov -a plugins/enttecdmxusbout/unix/coverage/openbase.info \
-        -a plugins/enttecdmxusbout/unix/coverage/opentest.info \
-        -o plugins/enttecdmxusbout/unix/coverage/openmerge.info
+lcov	-a coverage/dmxusbopenbase.info -a coverage/dmxusbopentest.info \
+	-o coverage/dmxusbopenmerge.info
 
 # DMXUSB Pro
-lcov -d plugins/enttecdmxusbout/unix/src -c -i -o plugins/enttecdmxusbout/unix/coverage/probase.info
+lcov -d plugins/enttecdmxusbout/unix/src -c -i -o coverage/dmxusbprobase.info
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:plugins/enttecdmxusbout/unix/src plugins/enttecdmxusbout/unix/test/pro/test_dmxusbpro
-lcov -d plugins/enttecdmxusbout/unix/src -c -o plugins/enttecdmxusbout/unix/coverage/protest.info
+lcov -d plugins/enttecdmxusbout/unix/src -c -o coverage/dmxusbprotest.info
 
-lcov -a plugins/enttecdmxusbout/unix/coverage/probase.info \
-        -a plugins/enttecdmxusbout/unix/coverage/protest.info \
-        -o plugins/enttecdmxusbout/unix/coverage/promerge.info
-
-# Generate combined HTML report
-lcov -a plugins/enttecdmxusbout/unix/coverage/openmerge.info \
-	-a plugins/enttecdmxusbout/unix/coverage/promerge.info \
-	-o plugins/enttecdmxusbout/unix/coverage/dmxusbout.info
-genhtml -o plugins/enttecdmxusbout/unix/coverage/html plugins/enttecdmxusbout/unix/coverage/dmxusbout.info
+lcov	-a coverage/dmxusbprobase.info -a coverage/dmxusbprotest.info \
+	-o coverage/dmxusbpromerge.info
 
 #############################################################################
 # Enttec Wing
 #############################################################################
-mkdir -p plugins/ewinginput/coverage/html
-
-lcov -d plugins/ewinginput/src --zerocounters
+lcov -d plugins/ewinginput/src -c -i -o coverage/ewingbase.info
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:plugins/ewinginput/src plugins/ewinginput/test/test_ewing
-lcov -d plugins/ewinginput/src -c -o plugins/ewinginput/coverage/ewinginput.info
+lcov -d plugins/ewinginput/src -c -o coverage/ewingtest.info
+
+lcov	-a coverage/ewingbase.info -a coverage/ewingtest.info \
+	-o coverage/ewingmerge.info
+
+#############################################################################
+# All combined and HTMLized
+#############################################################################
+
+lcov	-a coverage/enginemerge.info \
+	-a coverage/ewingmerge.info \
+	-a coverage/dmxusbopenmerge.info \
+	-a coverage/dmxusbpromerge.info \
+	-o coverage/coverage.info
 
 # Generate HTML report
-genhtml -o plugins/ewinginput/coverage/html plugins/ewinginput/coverage/ewinginput.info
+genhtml -o coverage/html coverage/coverage.info
+
