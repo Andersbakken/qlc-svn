63e5953324d45850ffeae478ab7cf10e1c69a3e3
hjunnila <hjunnila@0db28fee-5c31-0410-96ea-e69cce70e049>
2010-09-19 09:03:50 +0000

    Removed ifs related to OSX differentiation, since there doesn't seem to be a common if syntax for unix & OSX bash shells.

--- unittest.sh
+++ unittest.sh
@@ -5,11 +5,8 @@
 #############################################################################
 pushd .
 cd engine/test
-if [ `uname -r`=="Darwin" ]; then
-	DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:../src ./test_engine
-else
+DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:../src \
 	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../src ./test_engine
-fi
 RESULT=$?
 if [ $RESULT != 0 ]; then
 	echo "Engine unit test failed ($RESULT). Please fix before commit."
@@ -37,11 +34,8 @@ popd
 
 pushd .
 cd plugins/ewinginput/test
-if [ `uname -s` == "Darwin" ]; then
-	DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:../src ./test_ewing
-else
+DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:../src \
 	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../src ./test_ewing
-fi
 RESULT=$?
 if [ $RESULT != 0 ]; then
 	echo "Enttec wing unit test failed ($RESULT). Please fix before commit."
@@ -55,11 +49,8 @@ popd
 
 pushd .
 cd plugins/enttecdmxusbout/unix/test/pro
-if [ `uname -r`=="Darwin" ]; then
-	DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:../../src ./test_dmxusbpro
-else
+DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:../../src \
 	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../src ./test_dmxusbpro
-fi
 RESULT=$?
 if [ $RESULT != 0 ]; then
 	echo "Enttec DMXUSB Pro unit test failed ($RESULT). Please fix before commit."
@@ -73,11 +64,8 @@ popd
 
 pushd .
 cd plugins/enttecdmxusbout/unix/test/open
-if [ `uname -r`=="Darwin" ]; then
-	DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:../../src ./test_dmxusbopen
-else
+DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH:../../src \
 	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../src ./test_dmxusbopen
-fi
 RESULT=$?
 if [ $RESULT != 0 ]; then
 	echo "Enttec DMXUSB Open unit test failed ($RESULT). Please fix before commit."
